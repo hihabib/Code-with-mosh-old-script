ad6642479c9e814ac8a399f85e5baa847fd5564b
Wai Lwin Phyo <wailwinphyo.dev@gmail.com>
2020-07-16 16:03:54 +0630

    Added end index

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ jq.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
 jQuery.noConflict();
 
-var start_index = 0;
+var startIndex = 0; var endIndex = 0;
 var links = $("ul.section-list a.item");
 var data_obj = {};
 $("ul.section-list a.item").each(function(i ,obj){
-    var sec_name = $(this).parents(".course-section").find(".section-title").text().trim();
+    var sec_name = $(this).parents(".course-section").find(".section-title").text().replace(/\:/igm, ".").trim();
     var lecture_id = $(this).attr("id");
     var lecture_name = $(this).find(".lecture-name").text().replace(/(\r\n|\n|\r)\s*/igm, " ").trim();
     var obj = {
-    	id : i,
-    	sec_name : sec_name,
-    	lecture_name : lecture_name,
-    	getName : function(){
-    		return `${this.id}. ${this.sec_name}`;
-    	}
+        id : i,
+        sec_name : sec_name,
+        lecture_name : lecture_name,
+        getName : function(){
+            return `${this.id}. ${this.sec_name}`;
+        }
     }
     data_obj[`${lecture_id}`] = obj;
 });
@@ -24,21 +24,26 @@ console.log("Lecture Map constructed");
 console.log(data_obj);
 console.log("Downloading Started");
 
-var i = start_index * 2;
+var i = startIndex * 2;
+endIndex = endIndex == 0 ? links.length : endIndex;
 function myFunc(){
-	setTimeout(function(){
-		var index = Math.floor(i/2);
-		if (i % 2 == 0){
-			links[index].click();
-			myFunc();
-		}else if (index < links.length){
-			$("a.download").attr("data-x-origin-download-name", data_obj[`${links[index].id}`].getName() + " - " + $("a.download").attr("data-x-origin-download-name"));
-			console.log( $("a.download").attr("data-x-origin-download-name"));
-			var downloadBtn = document.querySelector("a.download");
-			if(downloadBtn != null) downloadBtn.click();
-			myFunc();
-		}
-	    ++i;
-	}, 10000);
+    setTimeout(function(){
+        var index = Math.floor(i/2);
+        if(index <= endIndex){
+            if (i % 2 == 0){
+                links[index].click();
+                myFunc();
+            } else {
+                $("a.download").attr("data-x-origin-download-name", data_obj[`${links[index].id}`].getName() + " - " + $("a.download").attr("data-x-origin-download-name"));
+                console.log( $("a.download").attr("data-x-origin-download-name"));
+                var downloadBtn = document.querySelector("a.download");
+                if(downloadBtn != null) downloadBtn.click();
+                myFunc();
+            }
+            ++i;
+        } else {
+            console.log(" ----- Finished Downloading ----- ");
+        }
+    }, 10000);
 }
 myFunc();
